aa21a776bb24bfb8a157595ea7df63746d3192ad
Rakesh Prabhakaran <rakesh.1985march@gmail.com>
2020-01-31 22:13:56 +1100

    * created a new package under practice to add all testing specific files * created teasing configuration xml * created navigation test class for testing

---
+++ src/test/java/Practice/practice/testng/TestNgTestClass.java
@@ -0,0 +1,38 @@
+package Practice.practice.testng;
+
+import Practice.pages.LandingPage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+public class TestNgTestClass {
+WebDriver driver;
+
+
+
+    @Test
+
+    public void testNavToCart() {
+
+        LandingPage landingPage = new LandingPage(driver);
+        landingPage.navigateToLanding();
+        Assert.assertEquals("Rahul Shetty Academy",driver.getTitle());
+
+    }
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.get("https://www.rahulshettyacademy.com/");
+    }
+    @AfterMethod
+    public void testDown(){
+        driver.close();
+        driver.quit();
+    }
+}
